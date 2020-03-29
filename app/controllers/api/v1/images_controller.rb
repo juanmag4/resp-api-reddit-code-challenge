1a0a6e7511fed70d3d597ee70fd7f06e10eedbcd
Juan Manuel Grimal <juanmag4@MacBook-Pro-de-Juan.local>
2020-03-29 13:23:53 -0300

    dismisse duplicated image urls and small refactor

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::ImagesController < ApplicationController
+  require 'set'
   $images = []
   def index
     render json: $images
   end
 
   def create
-    puts $images
-    $images.push(params['url'])
-    render json: 'Success'
+    response = {}
+    if $images.include?(params['url'])
+      response['code'] = 400
+      response['message'] = 'Image already saved'
+    else
+      $images.push(params['url'])
+      response['code'] = 200
+      response['message'] = 'Image saved'
+    end
+
+    render json: response
   end
 end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,29 +2,17 @@ class Api::V1::PostsController < ApplicationController
   # Get Posts
   def index
     url = createURL(params['limit'], params['count'], params['after'], params['before'])
-    result = api_request(:get, url)
+    result = getRequest(url)
     response = parseResponse(result)
     render json: response
   end
 
   private
 
-  def api_request(type , url, body=nil, header =nil )
+  def getRequest(url)
     require "net/http"
     uri = URI.parse(url)
-    case type
-    when :post
-      request = Net::HTTP::Post.new(uri)
-      request.body = body
-    when :get
-      request = Net::HTTP::Get.new(uri)
-    when :put
-      request = Net::HTTP::Put.new(uri)
-      request.body = body
-    when :delete
-      request = Net::HTTP::Delete.new(uri)
-    end
-    request.initialize_http_header(header)
+    request = Net::HTTP::Get.new(uri)
     request.content_type = 'application/json'
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') {|http| http.request request}
   end
