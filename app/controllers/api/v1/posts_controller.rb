2620d57bf898aec1414d719b73431d8b4f703435
Juan Manuel Grimal <juanmag4@MacBook-Pro-de-Juan.local>
2020-03-28 23:22:50 -0300

    query params and abstration of logic

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,20 +1,13 @@
 class Api::V1::PostsController < ApplicationController
   # Get Posts
   def index
-    require 'json'
-    posts = []
-    response = {}
-    result = api_request(:get, 'https://www.reddit.com/top.json?limit=50&count=0')
-    parsedResult = JSON.parse(result.body)
-    children = parsedResult['data']['children']
-    for value in children do
-      posts.push(value['data'])
-    end
-    response['after'] = parsedResult['data']['after']
-    response['before'] = parsedResult['data']['before']
-    response['posts'] = posts
+    url = createURL(params['limit'], params['count'], params['after'], params['before'])
+    result = api_request(:get, url)
+    response = parseResponse(result)
     render json: response
   end
+
+  private
 
   def api_request(type , url, body=nil, header =nil )
     require "net/http"
@@ -34,5 +27,37 @@ class Api::V1::PostsController < ApplicationController
     request.initialize_http_header(header)
     request.content_type = 'application/json'
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') {|http| http.request request}
+  end
+
+  def createURL(limit, count, after = nil, before = nil)
+    url = "https://www.reddit.com/top.json?limit=#{limit}&count=#{count}"
+
+    if after != nil
+      url = "#{url}&after#{after}"
+    end
+
+    if before != nil
+      url = "#{url}&before#{before}"
+    end
+
+    url
+  end
+
+  def parseResponse(result)
+    require 'json'
+    posts = []
+    response = {}
+    parsedResult = JSON.parse(result.body)
+    children = parsedResult['data']['children']
+
+    for value in children do
+      posts.push(value['data'])
+    end
+
+    response['after'] = parsedResult['data']['after']
+    response['before'] = parsedResult['data']['before']
+    response['posts'] = posts
+
+    response
   end
 end
